37ab2c0e2eb8634a431a2fe4d13e7e511426ed66
Dmytro Loginov <dmytrolgnv@gmail.com>
2024-01-14 18:37:04 +0200

    Homework6

---
+++ Homework 6/Homework 6/Program.cs
@@ -0,0 +1,35 @@
+﻿using Services;
+
+namespace Homework_6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var productStore = new ProductStore();
+            var productInStore = productStore.GetProducts();
+            var basket = new Basket();
+            var order = new Order(basket);
+
+            Console.WriteLine("Products in store:");
+            for (int i = 0; i < productInStore.Length; i++)
+            {
+                Console.WriteLine($"{i + 1}. {productInStore[i].Name} (${productInStore[i].Price})");
+            }
+
+            Console.WriteLine("Select products to add to basket (enter with a space):");
+            string inputProductsId = Console.ReadLine();
+            string[] selectedProducts = inputProductsId.Split(' ');
+
+            foreach (var selectedId in selectedProducts)
+            {
+                if (int.TryParse(selectedId, out int index) && index > 0 && index <= productInStore.Length)
+                {
+                    basket.AddProduct(productInStore[index - 1]);
+                }
+            }
+
+            order.GetBill();
+        }
+    }
+}
---
+++ Homework 6/Homework 6/Services/ProductStore.cs
@@ -0,0 +1,32 @@
+﻿using Models;
+
+namespace Services
+{
+    public class ProductStore
+    {
+        private readonly Product[] products;
+
+        public ProductStore()
+        {
+            products =
+            [
+            new Product { Id = 1, Name = "t-short", Price = 100 },
+            new Product { Id = 2, Name = "dress", Price = 110 },
+            new Product { Id = 3, Name = "sweatshirt", Price = 120 },
+            new Product { Id = 4, Name = "sweater", Price = 130 },
+            new Product { Id = 5, Name = "shirt", Price = 140 },
+            new Product { Id = 6, Name = "shorts", Price = 150 },
+            new Product { Id = 7, Name = "jeans", Price = 160 },
+            new Product { Id = 8, Name = "coat", Price = 170 },
+            new Product { Id = 9, Name = "socks", Price = 180 },
+            new Product { Id = 10, Name = "jacket", Price = 190 },
+            new Product { Id = 11, Name = "hat", Price = 200 }
+            ];
+        }
+
+        public Product[] GetProducts()
+        {
+            return products;
+        }
+    }
+}
