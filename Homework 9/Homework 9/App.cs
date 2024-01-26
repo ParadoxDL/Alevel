61566fcbbbaf1f714d3408d0bfd04302fb3556af
Dmytro Loginov <dmytrolgnv@gmail.com>
2024-01-26 18:44:09 +0200

    Homework9

---
+++ Homework 9/Homework 9/App.cs
@@ -0,0 +1,33 @@
+﻿using Homework_9.Services;
+
+namespace Homework_9
+{
+    class App
+    {
+        public static void Run()
+        {
+            FileService fileService = new FileService(@"C:\MyLogs");
+            Logger logger = new Logger(fileService);
+            Actions actions = new Actions(logger);
+
+            for (int i = 0; i < 100; i++)
+            {
+                Random random = new Random();
+                int randomType = random.Next(1, 4);
+
+                switch (randomType)
+                {
+                    case 1:
+                        actions.Info();
+                        break;
+                    case 2:
+                        actions.Warning();
+                        break;
+                    case 3:
+                        actions.Error();
+                        break;
+                }
+            }
+        }
+    }
+}
---
+++ Homework 9/Homework 9/BusinessException.cs
@@ -0,0 +1,7 @@
+﻿namespace Homework_9
+{
+    class BusinessException : Exception
+    {
+        public BusinessException(string message) : base(message) { }
+    }
+}
