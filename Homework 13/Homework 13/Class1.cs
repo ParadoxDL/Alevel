43eb8a77b0cdfc11d7f06818c3cf0e86ab830e34
DmytroLoginov <dmytrolgnv@gmail.com>
2024-02-16 21:03:35 +0200

    Add

---
+++ Homework 13/Homework 13/Class1.cs
@@ -0,0 +1,16 @@
+﻿namespace Homework_13
+{
+
+    internal class Class1
+    {
+        public delegate void ShowDelegate(bool result);
+        public static void DisplayShowDelegate(bool result, ShowDelegate showDelegate)
+        {
+            showDelegate(result);
+        }
+        public static int Multiply(int x, int y)
+        {
+            return x * y;
+        }
+    }
+}
