a49430a59f52151c5ef844c7ef6ac5e2a09d6b51
DmytroLoginov <dmytrolgnv@gmail.com>
2024-02-16 21:03:08 +0200

    r

--- Homework 5/Logger.cs
+++
@@ -1,9 +0,0 @@
-﻿using System;
-
-public class Logger
-{
-	public void HelloWorld()
-	{
-        Console.WriteLine("Hello, World!");
-    }
-}
--- Homework 5/Starter.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-public class Starter
-{
-	public Class1()
-	{
-	}
-}
