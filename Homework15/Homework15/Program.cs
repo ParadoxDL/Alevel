170781fb4a5e67e4bde807755f5219ffabc6e335
DmytroLoginov <dmytrolgnv@gmail.com>
2024-02-23 20:15:06 +0200

    Homework15

---
+++ Homework15/Homework15/Program.cs
@@ -0,0 +1,19 @@
+﻿using Services.Abstractions;
+using Services;
+
+namespace Homework15
+{
+    internal class Program
+    {
+        static async Task Main()
+        {
+            File.WriteAllText("Hello.txt", "Hello");
+            File.WriteAllText("World.txt", "World");
+
+            IFileService fileService = new FileService();
+            string result = await fileService.ConcatenateAsync();
+
+            Console.WriteLine(result);
+        }
+    }
+}
---
+++ Homework15/Homework15/Services/Abstractions/IFileService.cs
@@ -0,0 +1,9 @@
+﻿namespace Services.Abstractions
+{
+    internal interface IFileService
+    {
+        Task<string> ReadHelloAsync();
+        Task<string> ReadWorldAsync();
+        Task<string> ConcatenateAsync();
+    }
+}
